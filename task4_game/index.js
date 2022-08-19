b78f513569e01ec79ecc87e811724d850bd57701
natakers <n.kersnovskaya@gmail.com>
2022-08-19 19:07:08 +0300

    feat: add health selection and cooldown logic

--- task4_game/index.js
+++ task4_game/index.js
@@ -1,5 +1,6 @@
 const readlineSync = require('readline-sync');
-// let attempts = readlineSync.question('How many attempts do you want to use in the game? ');
+
+const health = [10, 20, 30]
 
 const monster = {
   maxHealth: 10,
@@ -12,6 +13,7 @@ const monster = {
       "physicArmorPercents": 20, // физическая броня
       "magicArmorPercents": 20,  // магическая броня
       "cooldown": 0,     // ходов на восстановление
+      "currentCooldown": 0,
     },
     {
       "name": "Огненное дыхание",
@@ -20,6 +22,7 @@ const monster = {
       "physicArmorPercents": 0,
       "magicArmorPercents": 0,
       "cooldown": 3,
+      "currentCooldown": 0,
     },
     {
       "name": "Удар хвостом",
@@ -28,12 +31,13 @@ const monster = {
       "physicArmorPercents": 50,
       "magicArmorPercents": 0,
       "cooldown": 2,
+      "currentCooldown": 0,
     },
   ]
 }
 
 const wizard = {
-  maxHealth: 20,
+  maxHealth: null,
   name: "Евстафий",
   moves: [
     {
@@ -43,6 +47,7 @@ const wizard = {
       "physicArmorPercents": 0,
       "magicArmorPercents": 50,
       "cooldown": 0,
+      "currentCooldown": 0,
     },
     {
       "name": "Вертушка левой пяткой",
@@ -51,6 +56,7 @@ const wizard = {
       "physicArmorPercents": 0,
       "magicArmorPercents": 0,
       "cooldown": 4,
+      "currentCooldown": 0,
     },
     {
       "name": "Каноничный фаербол",
@@ -59,6 +65,7 @@ const wizard = {
       "physicArmorPercents": 0,
       "magicArmorPercents": 0,
       "cooldown": 3,
+      "currentCooldown": 0,
     },
     {
       "name": "Магический блок",
@@ -67,6 +74,7 @@ const wizard = {
       "physicArmorPercents": 100,
       "magicArmorPercents": 100,
       "cooldown": 4,
+      "currentCooldown": 0,
     },
   ]
 }
@@ -76,23 +84,24 @@ function battle() {
     attack()
   }
   if (monster.maxHealth > 0) {
-    console.log('Monster is win');
-  } else console.log('Wizard is win');
+    console.log('\nМонстр победил!');
+  } else console.log('\nМаг победил!');
 }
 
 function attack() {
-  let attack = Math.floor(Math.random() * monster.moves.length);
-  console.log(attack);
-  let wizardAttack = readlineSync.question('Chose attack: 0 1 2 3');
-  console.log(wizardAttack);
+  decreaseCooldown()
+  let attack = monsterMove()
+  if (monster.moves[attack]["cooldown"] > 0) {
+    monster.moves[attack]["currentCooldown"] = monster.moves[attack]["cooldown"]+1;
+  }
+  let wizardAttack = wizardMove()
   if (wizard.moves[wizardAttack]["cooldown"] > 0) {
-    wizard.moves[wizardAttack]["currentCooldown"] = wizard.moves[wizardAttack]["cooldown"];
+    wizard.moves[wizardAttack]["currentCooldown"] = wizard.moves[wizardAttack]["cooldown"]+1;
   }
   if (wizard.moves[wizardAttack]["physicArmorPercents"] <= monster.moves[attack]["physicalDmg"]) {
     wizard.maxHealth = wizard.maxHealth - monster.moves[attack]["physicalDmg"]
   }
   if (wizard.moves[wizardAttack]["magicArmorPercents"] <= monster.moves[attack]["magicDmg"]) {
-
     wizard.maxHealth = wizard.maxHealth - monster.moves[attack]["magicDmg"]
   }
   if (monster.moves[attack]["magicArmorPercents"] <= wizard.moves[wizardAttack]["magicDmg"]) {
@@ -101,8 +110,43 @@ function attack() {
   if (monster.moves[attack]["physicArmorPercents"] <= wizard.moves[wizardAttack]["physicalDmg"]) {
     monster.maxHealth = monster.maxHealth - wizard.moves[wizardAttack]["physicalDmg"]
   }
-  console.log('wizard.maxHealth ' + wizard.maxHealth);
-  console.log('monster.maxHealth ' + monster.maxHealth);
+  console.log(`Монстр наносит атаку ${monster.moves[attack].name}`);
+  console.log(`Маг наносит атаку ${wizard.moves[wizardAttack].name}`);
+  console.log('Здоровье мага ' + wizard.maxHealth);
+  console.log('Здоровье монстра ' + monster.maxHealth);
 }
 
+const monsterMove = () => {
+  let attack = Math.floor(Math.random() * monster.moves.length);
+  if (monster.moves[attack]["currentCooldown"] === 0) {
+    return attack
+  } else return monsterMove()
+}
+
+const wizardMove = () => {
+  let names = []
+  wizard.moves.forEach((move, index) => {
+    if (move["currentCooldown"] === 0) {
+      names.push(move.name)
+    }
+  })
+  let wizardAttackIndex = readlineSync.keyInSelect(names, 'Выберите атаку:');
+  return wizard.moves.findIndex(move => move.name === names[wizardAttackIndex])
+}
+
+const decreaseCooldown = () => {
+  monster.moves.forEach((move) => {
+    if (move["currentCooldown"] > 0) {
+      move["currentCooldown"]--
+    }
+  })
+  wizard.moves.forEach((move) =>  {
+    if (move["currentCooldown"] > 0) {
+      move["currentCooldown"]--
+    }
+  })
+}
+
+const wizardHealth = readlineSync.keyInSelect(health, 'Выберите показатель здоровья мага');
+wizard.maxHealth = health[wizardHealth]
 battle()
